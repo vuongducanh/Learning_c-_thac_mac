7c414277ad0a417654f38d74ee8c3f9eb444dd24
vuongducanh <anhvuong730@gamail.com>
2018-03-13 00:06:31 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OPP {
+    class OPP {
+
+        public abstract class Person {
+            public int age;
+           
+            //    abstract class có thể khai báo biến 
+            // và trong class abstract các phương thức có thể có phần thực thi; 
+            public abstract void Read ();
+            public abstract void Say ();
+            public   void Run () {
+                System.Console.WriteLine("nguoi chay");
+             }
+        }
+        class Student : Person {
+            //  class kế thừa abstract class thì phải có từ khóa override và bắt buộc thực thi tất cả các phương thúc của lớp cha
+             
+            public override void Read () {
+                System.Console.WriteLine ("student read .. ");
+            }
+            // public override void Run(){
+            //     System.Console.WriteLine("studen run");
+            // }
+            public override void Say () {
+                System.Console.WriteLine ("say ...");
+            }
+
+        }
+        public static void Main (string[] a) {
+             Student student1 = new Student();
+             student1.Run();
+        }
+    }
+}
+
+// using System;
+
+// namespace OPP
+// {
+//     class OPP
+//     {
+//         interface Irun
+//         {
+//             //một interface thì không thể khai báo biến;
+//             void run();
+//         }
+//         interface Ispeak
+//         {
+//             void Speak();
+//         }
+//         class Person:Irun,Ispeak
+//         {
+//             public void run()
+//             {
+//                 System.Console.WriteLine("person run ...");
+//             }
+//             public void Speak(){
+//                 System.Console.WriteLine("person speak ... ");
+//             }
+//         }
+//         public static void Main()
+//         {
+//             Person person1 = new Person();
+//             person1.Speak();
+//         }
+//     }
+// }
---
+++ README.md
@@ -0,0 +1,11 @@
+# interface and abstract class
+## Khac nhau:
+### Trong phương thức(method) của abstract class thì có phần thực thi  còn interface thì không có phần thực thi 
+### Interface Thì các class có thể  implementation  nhiều interface còn abstract thì chỉ thực thi một
+### abstract class có thể khai báo biến còn interface không thể khai báo biến
+### Chú ý : khi method của abstract có từ khóa abstract thì class kế thừa bắt buộc phải đè lên nó với từ khóa override
+
+## Giống nhau 
+### đều không khởi tạo được đối tượng bên trong được
+###  Abstract class và interface đều có thể khai báo các phương thức nhưng không thực hiện chúng.
+###  Abstract class và interface đều bao gồm các phương thức abstract.
